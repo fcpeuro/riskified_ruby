6efc99407d1d55f9dd828706b12f2589cd550465
Mahmoudz <mahmoud.al.zalat@cg.nl>
2019-04-04 14:50:33 +0200

    create custom api connection exception for the gem

---
+++ lib/riskified/Exceptions/api_connection_error.rb
@@ -0,0 +1,11 @@
+module Riskified
+  module Exceptions
+
+    class ApiConnectionError < StandardError
+      def initialize(msg = 'Something went wrong while connecting to the Riskified API.')
+        super
+      end
+    end
+
+  end
+end
