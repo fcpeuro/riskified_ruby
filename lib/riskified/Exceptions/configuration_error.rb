e503cdec5d688b1e74cf08c916cdcade9d09d77d
Mahmoudz <mahmoud.al.zalat@cg.nl>
2019-04-12 11:54:20 +0200

    validate required configurations are set or raise custom exception

---
+++ lib/riskified/Exceptions/configuration_error.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Riskified
+  module Exceptions
+
+    class ConfigurationError < StandardError
+    end
+
+  end
+end
--- lib/riskified/configuration.rb
+++ lib/riskified/configuration.rb
@@ -1,17 +1,19 @@
 module Riskified
   class Configuration
-    attr_accessor :sandbox_mode, :sync_mode, :auth_token, :default_referrer, :default_shop_domain
+    attr_accessor :sandbox_mode, :sync_mode, :auth_token, :default_referrer, :shop_domain
 
     def initialize
-      @sync_mode = true # set by default.
+      @sync_mode = true # is set by default.
       @sandbox_mode = nil
       @auth_token = nil
       @default_referrer = nil
-      @default_shop_domain = nil
+      @shop_domain = nil
     end
 
     def validate
-      raise Exception.new('The "RISKIFIED_AUTH_TOKEN" is not found.') if @auth_token.nil? || @auth_token.empty?
+      raise ConfigurationError.new('The "Auth Token" configuration is not found.') if @auth_token.nil? || @auth_token.empty?
+      raise ConfigurationError.new('The "Shop Domain" configuration is not found.') if @shop_domain.nil? || @shop_domain.empty?
+      raise ConfigurationError.new('The "Default Referrer" configuration is not found.') if @default_referrer.nil? || @default_referrer.empty?
     end
 
   end
