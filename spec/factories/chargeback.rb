06e8367783340ec74adf4b1f178da9951d67b618
Mateus Guerra <mateusg@unochapeco.edu.br>
2019-10-07 10:02:11 +0200

    Testing chargeback

---
+++ spec/factories/chargeback.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+
+  factory :chargeback, class: Riskified::Entities::ChargebackMessage do
+    id {rand(111..999)}
+    chargeback_details {
+      Riskified::Entities::ChargebackDetails.new(
+        id: nil,
+        chargeback_at: "2019-10-07",
+        chargeback_currency: "EUR",
+        chargeback_amount: 100,
+        reason_code: "no_reason",
+        type: "cb",
+        gateway: "credit_card",
+        reason_description: "no_reason",
+        mid: nil,
+        arn: nil,
+        credit_card_company: "visa",
+        respond_by: "2020-05-01",
+        card_issuer: "Testing Test"
+      )
+    }
+  end
+end
